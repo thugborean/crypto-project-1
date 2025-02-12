a1b1aef71c2cdbcf7e94673133a80448cdd5aa97
Borean <>
2025-02-12 16:52:16 +0100

    Rabin Miller finally completed

--- Main.py
+++ Main.py
@@ -1,48 +1,47 @@
 import random
 
-def rabinMiller(n) -> bool:
-    # begin  = 2
-    # end = n - 2
-    # s = n - 1
+def rabinMiller(n, iterations) -> bool:
+    # step 0, quick check
+    if n == 1 or n == 2 or n == 3: return True
+    elif n % 2 == 0 or n % 3 == 0 or n % 5 == 0: return False
 
-    # a = random.randInt(begin, end)
-    # x = pow(a,s,n)
-
-    # if x == 1 or x == n - 1:
-    #     return True 
-    
-    # for j = 1 to r = 1
-    
-    # step 1 - stop when m is not a while number
-    k = 1
-    m = 0
+    # step 1, stop when m is not a whole number
     newN = n - 1
+    k = 1    #r
+    m = newN #s
 
     res = 0
     while True:
         res = newN / (2**k)
-        if m == int(m):
-            k += 1
-            m = res
-            continue
-        else:
+
+        # check for decimal
+        if res % 1 != 0:  
             break
+        
+        k += 1
+        m = res
 
     # step 2, 1 < a < n -1, we need a and m
-    a = random.randint(1, newN)
+    a = random.randint(2, n - 2)
 
-    # b = a^m mod n
-    b = pow(a, int(m), int(n))
-    if b == 1 or b == -1:
-        return True
+    # step 3, x = a^m mod n
+    for i in range(iterations):
+        x = pow(a, int(m), int(n))
+        if x == 1 or x == n - 1:
+            continue
 
-    newB = pow(b, 2, int(n))
-    if newB == 1:
-        return False
-    elif newB == -1:
-        return True
+        is_composite = True
+        for j in range(k - 1):
+            x = pow(x, 2, n)  # x = x^2 % n, dubblar exponenten varje gång vi testar
+            if x == n - 1:
+                is_composite = False
+                break
 
-    return False
+        if is_composite:
+            return False  # Hittade en witness
+
+    return True
+
 
 def calculateD(p,q,e) -> int:
     # d = e^-1 * (mod (p − 1)(q − 1))
@@ -56,9 +55,11 @@ def main():
     q = 11
     e = 13
 
-    babbe = calculateD(p,q,e)
-    print(babbe)
-    res = rabinMiller(1)
-    print(res)
+    # res1 = calculateD(p,q,e)
+    # print(res1)
+
+    val = int(input("Input number to check if it's prime: "))
+    res2 = rabinMiller(val, 20)
+    print(res2)
 
 main()

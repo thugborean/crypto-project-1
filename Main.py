c9a8c78bbf6e05664727cce7adde62b0f86d0014
Borean <>
2025-02-14 19:56:22 +0100

    Danne push

--- Main.py
+++ Main.py
@@ -12,13 +12,12 @@ def generatePrimes(numbersToGenerate, sizeOfNumbersInBits, iterations):
         if isPrime: listOfPrimes.add(number)
     
     endTime = time.time()
-    print(f"Time elapsed: {endTime - startTime} seconds, to generate {numbersToGenerate} primes of size {sizeOfNumbersInBits}")
+    print(f"Time elapsed: {endTime - startTime} seconds, to generate {numbersToGenerate} primes of size {sizeOfNumbersInBits} bits")
 
 def rabinMiller(n, iterations) -> bool:
     # step 0, quick check
     if n == 2 or n == 3: return True
     if n < 2 or n % 2 == 0: return False
-
 
     # step 1, stop when m is not a whole number
     newN = n - 1
@@ -69,29 +68,23 @@ def extendedGCD(a, b) -> int:
     return gcd, x, y
 
 def main():
-    # p
-    a = 5411871029092462825516185209329155738882437723668937447678705813383753989896625756765214367131469274835377472645480091140402399461250152678125693930035771
-    # q
-    m = 9714555070505236732818155692536136618594137166972361243648893311925812214088974952419769198868507372533717366520228097147768109020620023156613694426379779
-    e = 2**16 + 1
-    # e * d / (mod (a - 1)(m - 1)) = 1
-    gcd, x, y = extendedGCD(a,m)
-    print(f"{gcd} + {x} + {y}")
+    # # p
+    # a = 5411871029092462825516185209329155738882437723668937447678705813383753989896625756765214367131469274835377472645480091140402399461250152678125693930035771
+    # # q
+    # m = 9714555070505236732818155692536136618594137166972361243648893311925812214088974952419769198868507372533717366520228097147768109020620023156613694426379779
+    # e = 2**16 + 1
+    # # e * d / (mod (a - 1)(m - 1)) = 1
+    # gcd, x, y = extendedGCD(a,m)
+    # print(f"{gcd} + {x} + {y}")
 
-
-    inverse = True
-    if (gcd != 1): inverse = False
-    print(inverse)
+    # inverse = True
+    # if (gcd != 1): inverse = False
+    # print(inverse)
 
     # res = calculateD(p,q,e)
     # print(res1)
 
-    # listOfPrimes = set()
-    # while len(listOfPrimes) < 100:
-    #     number = random.getrandbits(4096)
-    #     isPrime = rabinMiller(number, 20)
-    #     if isPrime: listOfPrimes.add(number)
-        
     # res3 = inverseModM(1,4)
     # print(res3)
+    generatePrimes(100, 4096, 20)
 main()
